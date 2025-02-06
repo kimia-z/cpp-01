a960df98d10fab182fbc145c4d63369ca70a7c34
kimia <kimiaziari1997@gmail.com>
2025-02-06 17:16:37 +0100

    add ex00

---
+++ ex00/main.cpp
@@ -0,0 +1,11 @@
+#include "Zombie.hpp"
+#include <iostream>
+
+int main()
+{
+	// Zombie zombie("zomb");
+	std::cout << "here!!!!!!!" << std::endl;
+	// newZambie()
+	// Zombie *heapZombie = newZambie("Heapi");
+	return 0;
+}
