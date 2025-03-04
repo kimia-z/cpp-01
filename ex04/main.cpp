7d785e7033bc9aee94829cf3e1e34d0f70d2edec
kimia <kimiaziari1997@gmail.com>
2025-03-04 16:18:08 +0100

    fix ex04

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 #include <fstream>
 
-int main(int argc, std::string *argv)
+int main(int argc, char **argv)
 {
 	if (argc != 4)
 	{
@@ -13,21 +13,37 @@ int main(int argc, std::string *argv)
 	std::string s1 = argv[2];
 	std::string s2 = argv[3];
 	
-	std::ifstream inputFile(filename.c_str());
-	if (!inputFile.is_open())
+	std::ifstream input(filename.c_str());
+	if (!input.is_open())
 	{
 		std::cerr << "can not open the input file!" << std::endl;
 		return 1;
 	}
-	std::ofstream outputFile((filename + ".replace").c_str());
-	if (!outputFile.is_open())
+	std::ofstream output((filename + ".replace").c_str());
+	if (!output.is_open())
 	{
 		std::cerr << "can not open the output file!" << std::endl;
 		return 1;
 	}
 	std::string line;
-	while(std::getline(inputFile, line))
+	while(std::getline(input, line))
 	{
-		
+		size_t	pos = 0;
+		size_t	prev_pos = 0;
+		std::string	add_line;
+		while (true)
+		{
+			pos = line.find(s1, prev_pos);
+			if (pos == std::string::npos)
+				break;
+			add_line += line.substr(prev_pos, pos - prev_pos);
+			add_line += s2;
+			prev_pos = pos + s1.length();
+		}
+		add_line += line.substr(prev_pos);
+		output << add_line << std::endl;
 	}
+	input.close();
+	output.close();
+	return 0;
 }
