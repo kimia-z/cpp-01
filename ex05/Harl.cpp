05f997793e97e9304eaad529c3055017e9ef81fa
kimia <kimiaziari1997@gmail.com>
2025-03-07 14:29:34 +0100

    fix ex05

--- ex05/Harl.cpp
+++ ex05/Harl.cpp
@@ -2,19 +2,22 @@
 
 void Harl::debug(void)
 {
-	std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do!"
-			<< std::endl;
+	std::cout << "I love having extra bacon for my"
+		<< " 7XL-double-cheese-triple-pickle-specialketchup burger. I really do!"
+		<< std::endl;
 }
 
 void Harl::info(void)
 {
-	std::cout << "I cannot believe adding extra bacon costs more money. You didnt put enough bacon in my burger! If you did, I wouldnt be asking for more!"
+	std::cout << "I cannot believe adding extra bacon costs more money.\n"
+			<< " You didnt put enough bacon in my burger! If you did, I wouldnt be asking for more!"
 			<< std::endl;
 }
 
 void Harl::warning(void)
 {
-	std::cout << "I think I deserve to have some extra bacon for free. Ive been coming for years whereas you started working here since last month."
+	std::cout << "I think I deserve to have some extra bacon for free.\n"
+			<< " Ive been coming for years whereas you started working here since last month."
 			<< std::endl;
 }
 
@@ -26,6 +29,7 @@ void Harl::error(void)
 
 void Harl::complain(std::string level)
 {
+	std::string levels[4] = {"DEBUG", "INFO", "WARNING", "ERROR"};
 	void (Harl::*class_ptr[4]) (void) =
 	{
 		&Harl::debug,
@@ -33,5 +37,17 @@ void Harl::complain(std::string level)
 		&Harl::warning,
 		&Harl::error
 	};
-	int levels[4];
+	int flag = 5;
+	for (int i=0; i<4; i++)
+	{
+		if (level == levels[i])
+		{
+			flag = i;
+			break;
+		}
+	}
+	if (flag == 5)
+		std ::cout << "UNKNOWN LEVEL!" << std::endl;
+	else
+		(this->*class_ptr[flag])();
 }
